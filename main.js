c757cf3643d60e1487545f33422222990b498ec3
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-09 18:57:38 -0500

    Display all chatroom messages on load, prepare textbox

--- main.js
+++ main.js
@@ -14,66 +14,89 @@ const db = new sqlite3.Database("database.sql");
 //     cert: fs.readFileSync("../domain.cert.pem"),// path to ssl certificate from Porkbun
 // };
 
-db.serialize(() => {
+// for your information there is also db.each, which gives rows one-by-one
+db.all("SELECT name FROM sqlite_master WHERE type='table' AND name='chatroom_Landing';", (err, rows) => {
+	
+	console.log(rows);
 
-	db.all("SELECT name FROM sqlite_master WHERE type='table' AND name='chatroom_Landing';", (err, rows) => {
-        
-		console.log(rows);
+	if (err) {
+		console.error("Error A.");
+		process.exit();
+	}
 
-		if (err) {
-			console.error("Error.");
-			process.exit();
-		}
+	let hasLanding = false;
 
-		let hasLanding = false;
+	for (let row of rows) {
 
-		rows.forEach((row) => {
-			if (row.name == "chatroom_Landing") {
-				hasLanding = true;
-			}
-		});
+		if (row.name == "chatroom_Landing")
+			hasLanding = true;
+	}
 
-		if (!hasLanding)
+	if (!hasLanding) {
+
+		db.serialize(() => {
+
 			db.run("CREATE TABLE chatroom_Landing (message_id INTEGER PRIMARY KEY, message TEXT NOT NULL);");
-    });
-
-    // const stmt = db.prepare("INSERT INTO lorem VALUES (?);");
-    // for (let i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    // }
-    // stmt.finalize();
-
-    // db.each("SELECT rowid AS id, info FROM lorem;", (err, row) => {
-    //     console.log(row.id + ": " + row.info);
-    // });
+		
+			const stmt = db.prepare("INSERT INTO chatroom_Landing VALUES (NULL, ?);");
+			stmt.run("Welcome to the chatroom! We currently support NO link embedding!");
+			stmt.run("You can't type in this chatroom, but you can select chatrooms at the top that you CAN type in.");
+			stmt.finalize();
+		});
+	}
 });
 
 createServer((req, res) => { // options before () for https
 
 	console.log("\x1b[32m" + req.method + "\x1b[0m \x1b[2m" + req.url + "\x1b[0m");
 
-	// if (req.method == "POST" && req.url == "/") {}
+	if (req.method == "GET" && req.url == "/") {
+		req.url = "/Landing";
+	}
 
 	// reply
-	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-	res.end(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta charset="UTF-8">
-			<title>Chat Rooms</title>
-		</head>
-		<body>
-			<div>
-				Logged in as <strong>username123</strong> [settings] [log out] (settings let you change password, pfp, etc)
-			</div>
-			<nav>
-				Chatrooms: [<a href>Landing</a>] [<a href>General</a>]
-			</nav>
-			<h1>Landing</h1>
-		</body>
-		</html>
-	`);
+	db.all("SELECT * FROM chatroom_Landing;", (err, rows) => {
+
+		if (err) {
+			console.error("Error B.");
+			process.exit();
+		}
+
+		let messages = "";
+
+		for (let row of rows)
+			messages += row.message + "<br><br>";
+
+		res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+		res.end(`
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>Chat Rooms</title>
+			</head>
+			<body>
+				<div>
+					Logged in as <strong>username123</strong> [<a href>settings</a>] [<a href>log out</a>] (settings let you change password, pfp, etc)
+				</div>
+				<nav>
+					Chatrooms: [<a href>Landing</a>] [<a href>General</a>]
+				</nav>
+				
+				<h1>Landing</h1>
+				<hr>
+				<div id="messages">` + messages + `</div>
+
+				<form action="I wanna send a message" method="POST" target="hidden_iframe">
+					<input type="text" id="message" name="message">
+					<button type="submit">Send</button>
+				</form>
+				<iframe name="hidden_iframe" style="display: none;"></iframe>
+
+			</body>
+			</html>
+		`);
+	});
 
 }).listen(3000, "localhost", () => { // 443 for HTTPS
 
