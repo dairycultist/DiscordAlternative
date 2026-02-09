152c42e67448c2ad1febdd5630a099fb0a123c8b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-09 18:42:03 -0500

    Begin implementing SQLite

--- main.js
+++ main.js
@@ -4,9 +4,51 @@
 // so that we don't need extensive moderation tools, I think we should have a simple account system
 
 const fs = require("fs");
-const { createServer } = require("node:http");
+const { createServer } = require("node:http"); // switch to https later
 
-createServer((req, res) => {
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.sql");
+
+// const options = {
+//     key: fs.readFileSync("../private.key.pem"), // path to ssl PRIVATE key from Porkbun
+//     cert: fs.readFileSync("../domain.cert.pem"),// path to ssl certificate from Porkbun
+// };
+
+db.serialize(() => {
+
+	db.all("SELECT name FROM sqlite_master WHERE type='table' AND name='chatroom_Landing';", (err, rows) => {
+        
+		console.log(rows);
+
+		if (err) {
+			console.error("Error.");
+			process.exit();
+		}
+
+		let hasLanding = false;
+
+		rows.forEach((row) => {
+			if (row.name == "chatroom_Landing") {
+				hasLanding = true;
+			}
+		});
+
+		if (!hasLanding)
+			db.run("CREATE TABLE chatroom_Landing (message_id INTEGER PRIMARY KEY, message TEXT NOT NULL);");
+    });
+
+    // const stmt = db.prepare("INSERT INTO lorem VALUES (?);");
+    // for (let i = 0; i < 10; i++) {
+    //     stmt.run("Ipsum " + i);
+    // }
+    // stmt.finalize();
+
+    // db.each("SELECT rowid AS id, info FROM lorem;", (err, row) => {
+    //     console.log(row.id + ": " + row.info);
+    // });
+});
+
+createServer((req, res) => { // options before () for https
 
 	console.log("\x1b[32m" + req.method + "\x1b[0m \x1b[2m" + req.url + "\x1b[0m");
 
@@ -15,25 +57,25 @@ createServer((req, res) => {
 	// reply
 	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 	res.end(`
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Chat Rooms</title>
-</head>
-<body>
-	<div>
-		Logged in as <strong>username123</strong>
-	</div>
-	<nav>
-		Chatrooms: [<a href>Landing</a>] [<a href>General</a>]
-	</nav>
-	<h1>Landing</h1>
-</body>
-</html>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Chat Rooms</title>
+		</head>
+		<body>
+			<div>
+				Logged in as <strong>username123</strong> [settings] [log out] (settings let you change password, pfp, etc)
+			</div>
+			<nav>
+				Chatrooms: [<a href>Landing</a>] [<a href>General</a>]
+			</nav>
+			<h1>Landing</h1>
+		</body>
+		</html>
 	`);
 
-}).listen(3000, "localhost", () => {
+}).listen(3000, "localhost", () => { // 443 for HTTPS
 
 	// open page automatically
 	console.log(`Hosting on http://localhost:3000/`);
